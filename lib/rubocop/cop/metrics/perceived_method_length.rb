2f0a492eeaadd09383496b846f958df1846f0a82
KaiqueBressi <kaique_bressi@hotmail.com>
2019-07-03 20:40:01 -0300

    initial commit

---
+++ lib/rubocop/cop/metrics/perceived_method_length.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+# TODO: when finished, run `rake generate_cops_documentation` to update the docs
+module RuboCop
+  module Cop
+    module Metrics
+      # This cop checks if the number of statements of the method exceeds some maximum value.
+      # The maximum allowed statement number is configurable.
+
+      class PerceivedMethodLength < Cop
+        LABEL = 'Method'
+        MSG = '%<label>s has too many statements. [%<length>d/%<max>d]'
+
+        def on_def(node)
+          @statements = 0
+
+          return unless count_statements(node) > max_statements
+
+          add_offense(node, location: :name, message: message(@statements, max_statements))
+        end
+
+        private
+
+        def message(length, max_length)
+          format(MSG, label: cop_label, length: length, max: max_length)
+        end
+
+        def max_statements
+          cop_config['Max']
+        end
+
+        def count_statements(node)
+          return unless node
+
+          if compound_statement?(node)
+            node.node_parts.each(&method(:count))
+          else
+            count(node)
+          end
+
+          @statements
+        end
+
+        def count(node)
+          if has_body?(node)
+            @statements += 1
+
+            count_statements(extract_body(node))
+          else
+            @statements += 1
+          end
+        end
+
+        def compound_statement?(node)
+          node.type == :begin || node.type == :kwbegin
+        end
+
+        def extract_body(node)
+          if has_body?(node)
+            node.body
+          else
+            node
+          end
+        end
+
+        def has_body?(node)
+          node.respond_to?(:body)
+        end
+
+        def cop_label
+          LABEL
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/metrics/perceived_method_length_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Metrics::PerceivedMethodLength, :config do
+  subject(:cop) { described_class.new(config) }
+
+  let(:cop_config) { { 'Max' => 1 } }
+
+  context 'when method is an instance method' do
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        def m
+            ^ Method has too many statements. [3/1]
+           if a
+              [
+                 1,
+                 2,
+                 3,
+                 4
+              ]
+           end
+        end
+      RUBY
+    end
+  end
+end
